ef1968f57548725f89db86515ac00bbd45281982
Ji Xudong <jixudong99@163.com>
2023-04-13 23:11:15 +0800

    feat: Complete Tabbar development

---
+++ pages/my/my.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    My
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
