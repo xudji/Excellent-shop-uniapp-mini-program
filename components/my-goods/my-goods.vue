df301cd9d18002c73ed14530eb49ac7358fd1de3
Ji Xudong <jixudong99@163.com>
2023-04-16 17:08:28 +0800

    feat: complete the dev of the product list page

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,75 @@
+<template>
+  <view class="goods-item">
+    <!-- 左侧盒子 -->
+    <view class="goods-item-left">
+      <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+    </view>
+    <!-- 右侧盒子 -->
+    <view class="goods-item-right">
+      <view class="goods-name">
+        {{goods.goods_name}}
+      </view>
+      <view class="goods-info-box">
+        <view class="goods-price">
+          ￥{{goods.goods_price | tofixed}}
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: "my-goods",
+    props: {
+      goods: {
+        type: Object,
+        default: {}
+      }
+    },
+    data() {
+      return {
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    },
+    filters: {
+      tofixed(num) {
+        return Number(num).toFixed(2)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
+
+    .goods-item-left {
+      margin-right: 5px;
+
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: block;
+      }
+    }
+
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+
+      .goods-name {
+        font-size: 13px
+      }
+
+      .goods-info-box {
+        .goods-price {
+          color: #c00000;
+          font-size: 16px
+        }
+      }
+    }
+  }
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    goods_list
+    <view class="goods-list">
+      <view v-for="(goods, i)  in goodsList" :key="i" @click="gotoDetial(goods)">
+        <my-goods :goods="goods"></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,15 +12,68 @@
   export default {
     data() {
       return {
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0,
+        isLoading: false
 
       }
     },
-    methods: {
 
+    methods: {
+      gotoDetial(goods) {
+        uni.navigateTo({
+          url: '/subpkg/goods_detial/goods_detial?goods_id=' + goods.goods_id
+        })
+      },
+      // 获取商品列表数据  
+      async getGoodsList(cb) {
+        this.isLoading = true
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        // 关闭节流阀
+        this.isLoading = false
+        cb && cb()
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      }
+    },
+    onReachBottom() {
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕')
+      if (this.isLoading) return
+      // 让页面值+1
+      this.queryObj.pagenum++
+      // 重新获取数据
+      this.getGoodsList()
+    },
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    onPullDownRefresh() {
+      // 重置关键数据
+      // 重新发起数据请求
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isLoading = false
+      this.goodsList = []
+      // 重新发起数据请求
+      this.getGoodsList(() =>
+        uni.stopPullDownRefresh()
+      )
     }
+
   }
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
